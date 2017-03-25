7727bf30b1cb12f13025309537386c00e7aee220
郑俊鑫 <18810544291@163.com>
2017-03-25 16:42:55 +0800

    转换策略

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ function setProgress(progress, total) {
 function registerSW() {
     if('serviceWorker' in navigator) {
       navigator.serviceWorker
-               .register('/sw.js')
+               .register('sw.js')
                .then(function() { console.log("Service Worker Registered"); });
     }
 }
