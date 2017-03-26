ed52cdd8f1cf5410fe0802dd741e7d6e3e2e29c5
郑俊鑫 <18810544291@163.com>
2017-03-26 16:21:11 +0800

    增加注释

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ this.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('v2').then(function(cache) {
             return cache.addAll([
-            '/WALL-E/', // 对应项目 url，所以没有资源也要缓存
-            '/WALL-E/index.html',
+            '/WALL-E/', // 访问目录时，返回了 html 文件，所以需要缓存这个请求
+            // '/WALL-E/index.html', 对 html 的请求反而是不用的
             '/WALL-E/app.js',
             '/WALL-E/images/wallpaper1.jpg',
             ])
