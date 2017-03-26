6be38563bfbd6ed4473acb949be48f1557da64d1
郑俊鑫 <18810544291@163.com>
2017-03-26 10:37:07 +0800

    单独增加 / 缓存整个目录

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v2').then(function(cache) {
       return cache.addAll([
+        '/',
         'index.html',
         'app.js',
         'images/wallpaper1.jpg',
